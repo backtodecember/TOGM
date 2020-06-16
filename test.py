80a2fea67f429af0b70c31da12df1c039fa2d65e
motion <motion@Daneel.cs>
2020-06-16 17:30:49 -0500

    fixed pid tracked initial guess

--- test.py
+++ test.py
@@ -12,13 +12,22 @@ from klampt.io import loader
 
 
 
-x = np.load('results/PID_trajectory/3/x_init_guess.npy')
-u = np.load('results/PID_trajectory/3/u_init_guess.npy')
-t = np.load('results/PID_trajectory/3/time_init_guess.npy')
+# x = np.load('results/PID_trajectory/3/x_init_guess.npy')
+# u = np.load('results/PID_trajectory/3/u_init_guess.npy')
+# t = np.load('results/PID_trajectory/3/time_init_guess.npy')
 
-print('time list length',np.shape(t))
-print('x[0]:',x[0])
-print('x[-1]:',x[-1])
-print('u[0]:',u[0])
-print('t[0]',t[0])
-print('t[-1]:',t[-1])
+# print('time list length',np.shape(t))
+# print('x[0]:',x[0])
+# print('x[-1]:',x[-1])
+# print('u[0]:',u[0])
+# print('t[0]',t[0])
+# print('t[-1]:',t[-1])
+
+#traj_guess = np.hstack((np.load('results/PID_trajectory/2/q_init_guess.npy'),np.load('results/PID_trajectory/2/q_dot_init_guess.npy')))
+u_guess = np.load('results/PID_trajectory/2/u_init_guess.npy')
+x_guess = np.load('results/PID_trajectory/2/x_init_guess.npy')
+
+print(np.shape(u_guess),np.shape(x_guess))
+
+# np.save('results/PID_trajectory/2/x_init_guess.npy',x)
+# np.save('results/PID_trajectory/2/u_init_guess.npy',u)
